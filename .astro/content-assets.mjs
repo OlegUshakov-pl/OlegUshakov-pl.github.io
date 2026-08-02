744fd5980c6ffa4bdcf84b566a4fffe22b00a452
Oleg Ushakov <olegushakov38@gmail.com>
2026-08-02 21:19:41 +0200

    Update content-assets.mjs

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,1 +1,4 @@
-export default new Map();
+
+import __ASTRO_IMAGE_IMPORT_ZGlpMB from "images.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fhello.md";
+export default new Map([["images.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fhello.md", __ASTRO_IMAGE_IMPORT_ZGlpMB]]);
+		
