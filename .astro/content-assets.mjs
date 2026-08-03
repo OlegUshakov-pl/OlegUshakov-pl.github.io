b74f8a4b71a4aba28afdbc573afdb43def55f1a4
Oleg Ushakov <olegushakov38@gmail.com>
2026-08-03 05:49:20 +0200

    Add new PDF and image files to the public/images/pdf and public/images directories
    - Added Gallery Part1.pdf (5198 KB)
    - Added Gallery Part3.pdf (1300 KB)
    - Added Gallery Part4.pdf (23101 KB)
    - Added sonic.jpg (24 bytes)

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,4 +1,1 @@
-
-import __ASTRO_IMAGE_IMPORT_ZGlpMB from "images.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fhello.md";
-export default new Map([["images.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fposts%2Fhello.md", __ASTRO_IMAGE_IMPORT_ZGlpMB]]);
-		
+export default new Map();
