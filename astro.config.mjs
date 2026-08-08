44b7f5d847d6b51e97f7b2011980ce915e23001f
Oleg Ushakov <olegushakov38@gmail.com>
2026-08-08 09:35:44 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
+import keystatic from '@keystatic/astro';
 
 export default defineConfig({
   site: 'https://olegushakov-pl.github.io',
@@ -8,5 +9,7 @@ export default defineConfig({
   integrations: [
     react(),
     markdoc(),
+    // Keystatic только локально (не в production)
+    ...(process.env.NODE_ENV === 'production' ? [] : [keystatic()]),
   ],
 });
